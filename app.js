5d5b78b8174a164503ec30f05d2775137be71a25
Jenny Knuth <inkboco@gmail.com>
2018-04-10 21:12:34 -0600

    add files

---
+++ app.js
@@ -0,0 +1,102 @@
+
+// configuration for a line chart with error bars
+
+var lineChartWithErrorBars = function (plot, variance, title){
+  var chartConfig = {
+    title: {
+      text: title,
+    },
+    xAxis: {
+      type: 'datetime',
+      dateTimeLabelFormats: {
+          month: '%b \ %Y',
+      },
+      labels: {
+        y: 25,
+      }
+    },
+    yAxis: {
+        title: {
+          useHTML: true,
+          text: 'W/m<sup>2</sup>'
+        }
+    },
+    series: [
+      {
+        type: 'line',
+        name: 'total solar irradiance',
+        marker: {
+          enabled: true,
+          radius: 3,
+        },
+        data: plot,
+      },
+      {
+        type: 'errorbar',
+        name: 'uncertainty',
+        data: variance,
+      },
+    ]
+  };
+  return chartConfig;
+};
+
+// array of some available data sets
+var dataSets = [
+  'tcte_tsi_24hr.jsond?time,tsi_true_earth,measurement_uncertainty_true_earth',
+  'nrl2_observational_composite_tsi_v02r01.jsond?time,tsi,tsi_uncertainty'
+];
+// reusable attributes of data sets
+function getPrefix (dataSet) {
+  return dataSet.substr(0, dataSet.indexOf('.'));
+}
+function getUrl (dataSet) {
+  return 'http://lasp.colorado.edu/lisird/latis/' + dataSet + '&stride(10) ';
+}
+function getTitlePath (dataSet) {
+  var parameters = dataSet.slice(dataSet.indexOf('?'));
+  var titleData = parameters.split(',')[1];
+  return titleData;
+}
+// get reusable attributes of data sets
+function getDataSetInfo (index) {
+  return {
+    prefix: getPrefix(dataSets[index]),
+    url: getUrl(dataSets[index]),
+    titlePath: getTitlePath(dataSets[index]),
+  };
+}
+var counter = 0;
+document.getElementById('next').addEventListener('click', function(){
+     counter = (counter + 1) % 2;
+     var currentDataSet = getDataSetInfo(counter);
+     getData(currentDataSet);
+});
+getData(getDataSetInfo(0));
+
+// get data at specified url
+function getData(dataSet) {
+  axios.get(dataSet.url)
+  .then(function (response) {
+    var data = response.data[dataSet.prefix].data;
+
+    // create time and tsi array
+    var dataArray = data.map(function(item) {
+      return [item[0], item[1]];
+    });
+
+    // create uncertainty array and preserve null values
+    var uncertaintyArray = data.map(function(item) {
+      var uncertainty_low = item[1] - item[2] ? item[1] - item[2] : null;
+      var uncertainty_high = item[1] + item[2] ? item[1] + item[2] : null;
+      return [item[0], uncertainty_low, uncertainty_high];
+    });
+
+    var title = response.data[dataSet.prefix].metadata[dataSet.titlePath].long_name ?
+      response.data[dataSet.prefix].metadata[dataSet.titlePath].long_name : dataSet.titlePath.toUpperCase();
+
+    var myChart = Highcharts.chart('container', lineChartWithErrorBars(dataArray, uncertaintyArray, title));
+  })
+  .catch(function (error) {
+  });
+}
